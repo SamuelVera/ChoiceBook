a79d66a17f0870ff22787bc4a35355b66496daaa
SamuelVera <samuelvera1999@gmail.com>
2018-01-14 12:28:45 -0400

    Clase a partir de la cual se crea el objeto de los enemigos

---
+++ src/Choiceb/Enemigo.java
@@ -0,0 +1,41 @@
+package Choiceb;
+
+public class Enemigo{
+    private int HP;
+    private int ATK;
+    private int DEF;
+    private int SUE;
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getATK() {
+        return ATK;
+    }
+
+    public void setATK(int ATK) {
+        this.ATK = ATK;
+    }
+
+    public int getDEF() {
+        return DEF;
+    }
+
+    public void setDEF(int DEF) {
+        this.DEF = DEF;
+    }
+
+    public int getSUE() {
+        return SUE;
+    }
+
+    public void setSUE(int SUE) {
+        this.SUE = SUE;
+    }
+    
+}
