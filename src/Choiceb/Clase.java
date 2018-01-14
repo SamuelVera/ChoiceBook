676d87c76b764cb4c433ee80e025b9b3094802ab
SamuelVera <samuelvera1999@gmail.com>
2018-01-14 12:28:26 -0400

    Clase a partir de la cual se crea el objeto del personaje

---
+++ src/Choiceb/Clase.java
@@ -0,0 +1,70 @@
+package Choiceb;
+
+public class Clase {
+    
+    private int HP;
+    private int ATK;
+    private int DEF;
+    private int SAN;
+    private int SUE;
+    private char sexo;
+    private String NAME;
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getATK() {
+        return ATK;
+    }
+
+    public void setATK(int ATK) {
+        this.ATK = ATK;
+    }
+
+    public int getDEF() {
+        return DEF;
+    }
+
+    public void setDEF(int DEF) {
+        this.DEF = DEF;
+    }
+
+    public int getSAN() {
+        return SAN;
+    }
+
+    public void setSAN(int SAN) {
+        this.SAN = SAN;
+    }
+
+    public int getSUE() {
+        return SUE;
+    }
+
+    public void setSUE(int SUE) {
+        this.SUE = SUE;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+    
+    
+}
